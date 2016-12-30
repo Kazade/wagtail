1c46a9e750d4c61c1648f3b9dc9a4d159ca3d3b2
Christopher Smith <christopher@potatolondon.com>
2016-12-30 19:41:05 +0000

    Refactor get permissions utility methods in wagtailcore.permissions to use a common function to get all permission choices, allowing this logic to be used externally without using globals externally. Add function to filter permission choices to only include permissions with specified actions.

--- wagtail/wagtailcore/permissions.py
+++ wagtail/wagtailcore/permissions.py
@@ -7,13 +7,17 @@ site_permission_policy = ModelPermissionPolicy(Site)
 collection_permission_policy = ModelPermissionPolicy(Collection)
 
 
+def get_all_permission_choices():
+    global PERMISSIONS_LIST
+    return PERMISSIONS_LIST
+
+
 def get_app_permission_choices(app_label):
     """
     Return a list of permission form field choices pertaining to a given app.
     """
     import ipdb;ipdb.set_trace()
-    global PERMISSIONS_LIST
-    return [choice for choice in PERMISSIONS_LIST if choice[0].startswith('{}.'.format(app_label))]
+    return [choice for choice in get_all_permission_choices() if choice[0].startswith('{}.'.format(app_label))]
 
 
 def get_model_permission_choices(app_label, model_name):
@@ -22,12 +26,21 @@ def get_model_permission_choices(app_label, model_name):
     """
     model_name = model_name.lower()
     import ipdb;ipdb.set_trace()
-    global PERMISSIONS_LIST
 
-    model_perms = []
-    for choice in PERMISSIONS_LIST:
+    model_perm_choices = []
+    for choice in get_all_permission_choices():
         if (choice[0].startswith('{}.{}_'.format(app_label, model_name))
                 and choice[0].endswith("_{}".format(model_name))):
-            model_perms.append(choice)
+            model_perm_choices.append(choice)
 
-    return model_perms
+    return model_perm_choices
+
+
+def filter_permission_choices_to_actions(permission_choices, actions):
+    filtered_perm_choices = []
+    for choice in permission_choices:
+        for action in actions:
+            if choice[0].split('.')[1].startswith(action):
+                filtered_perm_choices.append(choice)
+
+    return filtered_perm_choices
