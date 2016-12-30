099de35905ab632945d940262f95f3a24110dfa7
Christopher Smith <christopher@potatolondon.com>
2016-12-30 17:58:04 +0000

    Fix get_model_permission_choices to properly check that a permission string's app_label and model_name

--- wagtail/wagtailcore/permissions.py
+++ wagtail/wagtailcore/permissions.py
@@ -22,4 +22,11 @@ def get_model_permission_choices(app_label, model_name):
     """
     import ipdb;ipdb.set_trace()
     global PERMISSIONS_LIST
-    return [choice for choice in PERMISSIONS_LIST if choice[0].startswith('{}.{}_'.format(app_label, model_name))]
+
+    model_perms = []
+    for choice in PERMISSIONS_LIST:
+        if (choice[0].startswith('{}.{}_'.format(app_label, model_name))
+                and choice[0].endswith("_{}".format(model_name))):
+            model_perms.append(choice)
+
+    return model_perms
